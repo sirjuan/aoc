9fcb7f6a51db47cbd14dbfdfee748e639311f16c
Juha Pekkarinen <juha.pekkarinen@outlook.com>
2024-12-20 22:39:20 +0200

    cleanup and optimize

--- challenges/20/solver.ts
+++ challenges/20/solver.ts
@@ -1,8 +1,11 @@
 import { PathNode, shortestPath } from '../../shared/shortestPath'
 import { parseMap, parseNumber, result, isExample } from '../../shared/utils'
 
-export const solver2: Solver = (inputStr) => {
+export const solver1: Solver = (inputStr) => {
   result(1, solve(inputStr, 2, isExample ? 1 : 100))
+}
+
+export const solver2: Solver = (inputStr) => {
   result(2, solve(inputStr, 20, isExample ? 50 : 100))
 }
 
@@ -14,51 +17,38 @@ function solve(inputStr: string, cheatDuration: number, threshold: number) {
     iterator: (char, x, y) => {
       if (char === '#') {
         walls.add(stringifyCoords([x, y]))
-      }
-      if (char === 'S') {
+      } else if (char === 'S') {
         startCoord[0] = x
         startCoord[1] = y
-      }
-      if (char === 'E') {
+      } else if (char === 'E') {
         endPoint = stringifyCoords([x, y])
       }
     },
   })
 
-  const isOutOfBounds = ([x, y]: Coord) => x <= 0 || y <= 0 || x >= map.width - 1 || y >= map.height - 1
-
-  const pathWithoutCheats = findShortestPath({ startCoord, endPoint, walls })
-  const traveledPoints = getPath(pathWithoutCheats)
+  const path = findShortestPath({ startCoord, endPoint, walls })
+  const points = getPath(path)
+  const indices = Object.fromEntries(points.map((point, index) => [point, index]))
 
   let result = 0
 
-  const indices: Record<string, number> = {}
-
-  traveledPoints.forEach((point, index) => {
-    indices[point] = index
-  })
-
-  traveledPoints.forEach((startPoint, index) => {
+  points.forEach((startPoint, startIndex) => {
     const startCoord = parseCoords(startPoint)
 
     for (let x = startCoord[0] - cheatDuration; x <= startCoord[0] + cheatDuration; x++) {
       for (let y = startCoord[1] - cheatDuration; y <= startCoord[1] + cheatDuration; y++) {
         const cheatCoord: Coord = [x, y]
-        if (isOutOfBounds(cheatCoord)) {
-          continue
-        }
-        const distance = manhattanDistance(startCoord, cheatCoord)
-        if (distance > cheatDuration) {
-          continue
-        }
-        const cheatPoint = stringifyCoords(cheatCoord)
-        if (walls.has(cheatPoint)) {
+        if (!map.inBounds(cheatCoord)) {
           continue
         }
 
-        const pointIndex = indices[cheatPoint]
-        const saved = pointIndex - distance - index
-        if (saved >= threshold) {
+        const distance = Math.abs(x - startCoord[0]) + Math.abs(y - startCoord[1])
+        if (distance > cheatDuration) {
+          continue
+        }
+
+        const cheatPoint = stringifyCoords(cheatCoord)
+        if (indices[cheatPoint] - distance - startIndex >= threshold) {
           result++
         }
       }
@@ -69,13 +59,6 @@ function solve(inputStr: string, cheatDuration: number, threshold: number) {
 }
 
 type Coord = [number, number]
-
-function parseNode(str: string) {
-  const [coordStr, ...cheatPoints] = str.split('|')
-  const coord = coordStr.split(',').map(parseNumber) as Coord
-  const cheatCoords = cheatPoints.map(parseCoords)
-  return { coord, cheatPoints, cheatCoords, point: coordStr }
-}
 
 function findShortestPath({
   startCoord,
@@ -88,23 +71,14 @@ function findShortestPath({
   walls: Set<string>
   target?: number
 }) {
-  const endCoord = parseNode(endPoint).coord
-
   return shortestPath({
     target,
     initial: stringifyCoords(startCoord),
-    isTarget: (pathNode) => parseNode(pathNode.point).point === endPoint,
-    edges: (pathNode) => {
-      const node = parseNode(pathNode.point)
-      const distance = manhattanDistance(node.coord, endCoord)
-      if (distance + pathNode.cost > target) {
-        return []
-      }
-
-      return getNeighbors(parseCoords(pathNode.point))
-        .filter(([x, y]) => x >= 0 && y >= 0 && !walls.has(stringifyCoords([x, y])))
-        .map((coord) => [stringifyCoords(coord), 1])
-    },
+    isTarget: (pathNode) => pathNode.point === endPoint,
+    edges: (pathNode) =>
+      getNeighbors(parseCoords(pathNode.point))
+        .filter((c) => !walls.has(stringifyCoords(c)))
+        .map((c) => [stringifyCoords(c), 1]),
   })[0]
 }
 
@@ -118,12 +92,8 @@ function getNeighbors(coord: Coord): Coord[] {
   ]
 }
 
-function manhattanDistance(coord1: Coord, coord2: Coord) {
-  return Math.abs(coord1[0] - coord2[0]) + Math.abs(coord1[1] - coord2[1])
-}
-
 function stringifyCoords(coord: Coord) {
-  return `${coord[0]},${[coord[1]]}`
+  return coord[0] + ',' + coord[1]
 }
 
 function parseCoords(str: string): Coord {
